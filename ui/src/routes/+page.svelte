575ea8c16d5524dbfaa99e0221646ed9cf629f1b
PentaMine <sasaovcaric@gmail.com>
2023-12-30 15:23:00 +0100

    frontend

---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Navbar from "../components/Navbar.svelte";
+</script>
+
+<header>
+    <Navbar/>
+</header>
+
+<!-- placeholder -->
+<div id="page">
+    <h1>Welcome to Encedeus</h1>
+
+    <p>Visit <a href="https://github.com/Encedeus/">our Github</a> for more info</p>
+</div>
+
+<style>
+    #page {
+        text-align: center;
+    }
+</style>
---
+++ ui/src/routes/documentation/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Navbar from "../../components/Navbar.svelte";
+</script>
+
+<header>
+    <Navbar/>
+</header>
+
+<h1>LOL JK</h1>
+<h2>nuh uh</h2>
